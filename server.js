19ee105d718fe7439a082bb166a217d22aa989bd
Brian Newsom <brian.newsom@colorado.edu>
2015-10-24 13:46:49 -0700

    Initial template

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ var app = express();
 // serve out of public folder
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
