54e0ebe2bdb51002d0339153445edbec12768072
Peyman Mortazavi <pey.mortazavi@gmail.com>
2015-10-25 02:35:24 -0700

    added test phone

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ io.on('connection', function(socket){
 		console.log(data)
 		var blob = JSON.parse(data)
 
-		var url = 'https://sandbox-api.venmo.com/v1/payments?phone=9704120347&amount=0.10&note=' + encodeURIComponent("For " + blob.job) + '&access_token=07a6297b15ab83f1a8e02feabd23bf0db21bb2c9f054db3833f2924bb62a228d'
+		var url = 'https://sandbox-api.venmo.com/v1/payments?phone=15555555555&amount=0.10&note=' + encodeURIComponent("For " + blob.job) + '&access_token=07a6297b15ab83f1a8e02feabd23bf0db21bb2c9f054db3833f2924bb62a228d'
 		console.log(url)
 
 		request.post('https://sandbox-api.venmo.com/v1/payments?phone=9704120347&amount=0.10&note=' + encodeURIComponent("For " + blob.job) + '&access_token=07a6297b15ab83f1a8e02feabd23bf0db21bb2c9f054db3833f2924bb62a228d', function (error, response, body) {
